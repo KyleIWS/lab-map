3a59a1b6b4d6a5adf5d606faf428fcee6358d536
kyle <kyleiws@uw.edu>
2017-05-02 15:44:09 -0700

    done with lab

---
+++ MapLab/app/src/main/java/edu/uw/kyleiws/maplab/MapsActivity.java
@@ -0,0 +1,69 @@
+package edu.uw.kyleiws.maplab;
+
+import android.graphics.Color;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        Marker DrumHell = googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(47.653793, -122.307824))
+                .title("Drumheller Fountain")
+                .snippet("Duck fam")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA)));
+
+        PolylineOptions dub = new PolylineOptions()
+                .add(
+                        new LatLng(47.654047, -122.308432),
+                        new LatLng(47.653561, -122.308032),
+                        new LatLng(47.654047, -122.307800),
+                        new LatLng(47.653561, -122.307600),
+                        new LatLng(47.654047, -122.307200));
+        dub.color(Color.MAGENTA);
+        dub.width(50);
+
+
+        Polyline polyline = googleMap.addPolyline(dub);
+
+        // Add a marker in MGH and move the camera
+        //LatLng sydney = new LatLng(-47, -122);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in MGHish"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
